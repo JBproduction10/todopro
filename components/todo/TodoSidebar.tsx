501e51ee9983039af23ecd9c20b34929ceef3a9f
Jonathan Bangala <jbangala90@gmail.com>
2025-06-01 15:45:47 +0200

    Fixing errors

--- components/todo/TodoSidebar.tsx
+++ components/todo/TodoSidebar.tsx
@@ -104,7 +104,7 @@ export function TodoSidebar({ open, onClose }: TodoSidebarProps) {
             <Plus className="h-3 w-3" />
           </Button>
         </div>
-        <div className="space-y-1 max-h-64 overflow-y-auto pr-1">
+        <div className="space-y-1 max-h-64 overflow-y-auto">
           {categories.map((category) => {
             const taskCount = getTasksByCategory(category.id).length
             return (
